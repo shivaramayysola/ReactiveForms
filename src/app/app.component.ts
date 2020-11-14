829f4c50179861fdcbe9b13cabdddc61c2298bf1
Shivaramayysola <shivaramayysola@gmail.com>
2020-11-14 16:41:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reactiveforms';
+
+get userName(){
+  return this.registrationForm.get('userName');
+}
+
+  constructor(private fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    userName: ['',[Validators.required,Validators.minLength(3)]],
+    password:[''],
+    confirmPassword:[''],
+    address: this.fb.group({
+      city:[''],
+      state:[''],
+      postalCode:['']
+    })
+  });
+
+
+  // registrationForm = new FormGroup({
+  //     userName: new FormControl('Shiva'),
+  //     password: new FormControl(''),
+  //     confirmPassword: new FormControl(''),
+  //     address:new FormGroup({
+  //       city: new FormControl(''),
+  //       state: new FormControl(''),
+  //       postalCode: new FormControl('')
+  //     })
+  // });
+
+  // loadApiData(){
+  //   this.registrationForm.patchValue({
+  //     userName:'Shiv',
+  //     password:'test',
+  //     confirmPassword:'test',
+     
+  //   });
+  // }
 }
